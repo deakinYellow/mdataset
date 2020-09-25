7a40e12141bda9032e62270c46b77c524e184be5
deakin <deakinhwang@163.com>
2020-09-25 11:09:49 +0800

    add-

--- bash/list.sh
+++ bash/list.sh
@@ -3,4 +3,4 @@
 #FILES_PATH=/media/deakin/00000354000386C6/work/dataset/casia-maxpy-clean/casia-maxpy-clean/CASIA-maxpy-clean
 FILES_PATH=/home/deakin/ml/data/data-hand-script/A
 
-python list_txt.py  --files_path=$FILES_PATH
+python ../list_txt.py  --files_path=$FILES_PATH
